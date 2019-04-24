8987c48d08c8a9e30a6ad02167bc8aba2c71d24d
Keresh <vuong@autonomous.nyc>
2019-04-24 14:28:36 +0700

    Add icons

--- src/components/HamburgerHeader/HamburgerHeader.js
+++ src/components/HamburgerHeader/HamburgerHeader.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View } from 'react-native';
 import PropTypes from 'prop-types';
+import Icons from 'react-native-vector-icons/MaterialIcons';
 
 const HamburgerHeader = ({ onPress }) => (
   <View>
-    <Button title='Menu' onPress={onPress} />
+    <Icons.Button
+      name='menu'
+      onPress={onPress}
+    />
   </View>
 );
 
