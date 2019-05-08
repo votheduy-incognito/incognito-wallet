d052c0e4b3f4c4618d34c3297f9a864c402b02f8
Keresh <vuong@autonomous.nyc>
2019-05-08 14:32:20 +0700

    Update TouchableOpacity core component

--- src/components/core/TouchableOpacity/Component.js
+++ src/components/core/TouchableOpacity/Component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TouchableOpacity as RNComponent } from 'react-native';
 
-const TouchableOpacity = (props) => <RNComponent {...props} />;
+const TouchableOpacity = (props) => <RNComponent activeOpacity={0.7} {...props} />;
 
 export default TouchableOpacity;
