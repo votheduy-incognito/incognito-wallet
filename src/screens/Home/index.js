a903de6d215ff78ba3a91ddb4028f56dca40b7a5
Keresh <vuong@autonomous.nyc>
2019-05-09 14:03:54 +0700

    Pass props from HomeContainer to Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Home from './Home';
 
-const HomeContainer = ({ defaultAccount }) => (
-  <Home account={defaultAccount} />
+const HomeContainer = ({ defaultAccount, ...otheeProps }) => (
+  <Home account={defaultAccount} {...otheeProps} />
 );
 
 const mapState = state => ({
@@ -12,7 +12,7 @@ const mapState = state => ({
 });
 
 HomeContainer.propTypes = {
-  defaultAccount: PropTypes.object
+  defaultAccount: PropTypes.object,
 };
 
 export default connect(mapState)(HomeContainer);
