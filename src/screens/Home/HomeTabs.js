ed5a7cc42f532d011e3f3d31be68c3152e9d4331
Keresh <vuong@autonomous.nyc>
2019-05-13 17:19:22 +0700

    Remove Token tab in Home

--- src/screens/Home/HomeTabs.js
+++ src/screens/Home/HomeTabs.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import { TabView } from '@src/components/core';
-import Token from '@src/screens/Token';
 import History from '@src/screens/History';
 import AccountDetail from '@src/screens/AccountDetail';
 
 this.tabData = [
-  {
-    key: 'tokens',
-    title: 'TOKENS',
-    screen: Token
-  },
   {
     key: 'history',
     title: 'HISTORY',
