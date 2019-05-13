2023e2d7b534fa6fd7f63beb344ce508e0891cfd
Keresh <vuong@autonomous.nyc>
2019-05-13 15:46:14 +0700

    Add dateTime format util

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,4 +1,5 @@
 import convert from '@src/utils/convert';
+import moment from 'moment';
 
 const AmountConstantFormat = new Intl.NumberFormat('en-US', {
   minimumFractionDigits: 2,
@@ -11,4 +12,7 @@ export default {
   amountMiliConstant(amount = throw new Error('Amount is required!')) { return AmountConstantFormat.format(amount); },
   amountConstant(amount = throw new Error('Amount is required!')) { return AmountConstantFormat.format(convert.toConstant(amount)); },
   amountToken(amount = throw new Error('Amount is required!')) { return AmountTokenFormat.format(Number.parseInt(amount) || 0); },
+  formatDateTime(dateTime, formatPartern) {
+    return moment(dateTime).format(formatPartern || 'DD/MM/YYYY - HH:mm:ss');
+  }
 };
