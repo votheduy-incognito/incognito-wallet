b6f83048e8487d1a810ec64ede61b7947c4588e3
Keresh <vuong@autonomous.nyc>
2019-05-13 14:15:41 +0700

    Update CopiableText

--- src/components/CopiableText/CopiableText.js
+++ src/components/CopiableText/CopiableText.js
@@ -5,9 +5,11 @@ import { Text, TouchableOpacity } from '@src/components/core';
 import clipboard from '@src/services/clipboard';
 import styleSheet from './style';
 
-const CopiableText = ({ text, textProps, containerProps, showCopyIcon = true, oneLine = false }) => (
-  <TouchableOpacity style={styleSheet.textBox} {...containerProps} onPress={() => clipboard.set(text)} >
-    <Text style={styleSheet.text} {...oneLine ? { numberOfLines: 1, ellipsizeMode: 'middle' } : {}} {...textProps} >{text}</Text>
+const CopiableText = ({ text, style, children, textProps, containerProps, showCopyIcon = true, oneLine = false }) => (
+  <TouchableOpacity style={[styleSheet.textBox, style]} {...containerProps} onPress={() => clipboard.set(text)} >
+    {
+      children || <Text style={styleSheet.text} {...oneLine ? { numberOfLines: 1, ellipsizeMode: 'middle' } : {}} {...textProps} >{text}</Text>
+    }
     { showCopyIcon && <MdIcons name='content-copy' size={20} style={styleSheet.copyIcon} />}
   </TouchableOpacity>
 );
@@ -17,7 +19,9 @@ CopiableText.propTypes = {
   textProps: PropTypes.object,
   containerProps: PropTypes.object,
   showCopyIcon: PropTypes.bool,
-  oneLine: PropTypes.bool
+  oneLine: PropTypes.bool,
+  children: PropTypes.node,
+  style: PropTypes.object
 };
 
 export default CopiableText;
