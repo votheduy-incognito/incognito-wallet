dec87bffd27f8289bd9bfd0214e94590ae4a438e
Keresh <vuong@autonomous.nyc>
2019-05-13 17:04:44 +0700

    Update RemoveAccount

--- src/screens/AccountDetail/index.js
+++ src/screens/AccountDetail/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { withNavigation } from 'react-navigation';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import { removeAccount } from '@src/redux/actions/account';
 import AccountDetail from './AccountDetail';
 
 const AccountDetailContainer = ({ account, ...otherProps}) => <AccountDetail {...otherProps} account={account}  />;
@@ -11,11 +12,13 @@ const mapState = state => ({
   account: state.account.defaultAccount
 });
 
+const mapDispatch = { removeAccount };
+
 AccountDetailContainer.propTypes = {
   account: PropTypes.object
 };
 
 export default compose(
-  connect(mapState),
+  connect(mapState, mapDispatch),
   withNavigation
 )(AccountDetailContainer);
