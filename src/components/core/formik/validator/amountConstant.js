1780ca0e6e2db9f79aff8450f17dc85f44eff886
0xkraken <hien.n@autonomous.nyc>
2019-05-14 11:45:01 +0700

    Update staking and defragment

--- src/components/core/formik/validator/amountConstant.js
+++ src/components/core/formik/validator/amountConstant.js
@@ -1,8 +1,10 @@
 import { number } from 'yup';
+import constant from '@src/constants/common';
 
 export default ({ max } = {}) => {
   const validate = number()
-    .required('Required!');
+    .required('Required!')
+    .min(constant.AMOUNT_CONSTANT_MIN);
 
   if (max){
     validate.lessThan(max);
