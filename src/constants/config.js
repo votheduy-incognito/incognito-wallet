e9bea2cec48a899dca400d7d158a0f145d6a7bf8
Keresh <vuong@autonomous.nyc>
2019-05-14 16:03:33 +0700

    Add new constant config

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,8 +1,26 @@
 import { API_BASE_URL, PASSWORD_SECRET_KEY, SHARD_ID, EXPLORER_CONSTANT_CHAIN_URL } from 'react-native-dotenv';
 
+const DEFAULT_LIST_SERVER = [{
+  id: 'local',
+  default: false,
+  address: 'http://localhost:9334',
+  username: '',
+  password: '',
+  name: 'Local'
+},
+{
+  id: 'testnet',
+  default: true,
+  address: 'http://test-node-constant-chain.constant.money:9334',
+  username: '',
+  password: '',
+  name: 'Testnet'
+}];
+
 export default {
   API_BASE_URL,
   PASSWORD_SECRET_KEY,
   SHARD_ID,
-  EXPLORER_CONSTANT_CHAIN_URL
+  EXPLORER_CONSTANT_CHAIN_URL,
+  DEFAULT_LIST_SERVER
 };
