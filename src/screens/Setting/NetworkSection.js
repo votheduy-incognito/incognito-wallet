18a5d4d0ce2aa00ac47d235f9c85ec09aad06926
Keresh <vuong@autonomous.nyc>
2019-05-14 16:44:07 +0700

    Show default network in NetworkSection setting

--- src/screens/Setting/NetworkSection.js
+++ src/screens/Setting/NetworkSection.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import MdIcons from 'react-native-vector-icons/MaterialIcons';
 import ROUTE_NAMES from '@src/router/routeNames';
+import serverService from '@src/services/wallet/Server';
 import Section from './Section';
 
 const NetworkSection = ({ navigation }) => {
+  const [ server, setServer ] = useState(null);
+  useEffect(() => {
+    serverService.getDefault()
+      .then(setServer);
+  }, []);
   const items = [
     {
-      title: 'Local',
-      desc: 'http://',
+      title: server?.name || 'Change default server',
+      desc: server?.address || '---',
       icon: <MdIcons name='laptop' size={24} />,
       handlePress: () => navigation?.navigate(ROUTE_NAMES.NetworkSetting)
     }
