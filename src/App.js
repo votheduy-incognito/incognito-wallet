dd1669625a7c8d55ddbebc44dc1e7c62fc474f67
Keresh <vuong@autonomous.nyc>
2019-05-15 14:25:20 +0700

    Make QrScanner to global

--- src/App.js
+++ src/App.js
@@ -12,12 +12,14 @@ import 'intl/locale-data/jsonp/en';
 import { Provider } from 'react-redux';
 import AppContainer from '@src/router';
 import configureStore from '@src/redux/store';
+import QrScanner from '@src/components/QrCodeScanner';
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <AppContainer />
+    <QrScanner />
   </Provider>
 );
 
