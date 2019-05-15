8c8da721bbadc6d80e9be440971fd5912b3bc3c3
Keresh <vuong@autonomous.nyc>
2019-05-15 15:08:29 +0700

    Add ScanQr code to SendConstant

--- src/screens/SendConstant/SendConstant.js
+++ src/screens/SendConstant/SendConstant.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
-import { Text, Container, Form, FormTextField, FormSubmitButton, Toast, ScrollView, CheckBoxField } from '@src/components/core';
+import { Text, Container, Form, FormTextField, FormSubmitButton, Toast, ScrollView, CheckBoxField, TouchableOpacity } from '@src/components/core';
 import { CONSTANT_COMMONS } from '@src/constants';
 import formatUtil from '@src/utils/format';
 import formValidate from './formValidate';
@@ -12,6 +12,9 @@ import { getEstimateFee } from '@src/services/wallet/RpcClientService';
 import convert from '@src/utils/convert';
 import common from '@src/constants/common';
 import ReceiptModal, { openReceipt } from '@src/components/Receipt';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { FONT } from '@src/styles';
+import { openQrScanner } from '@src/components/QrCodeScanner';
 
 const initialFormValues = {
   isPrivacy: false,
@@ -138,6 +141,12 @@ class SendConstant extends Component {
     return errors;
   }
 
+  handleQrScanAddress = () => {
+    openQrScanner(data => {
+      this.updateFormValues('toAddress', data);
+    });
+  }
+
   render() {
     const { account } = this.props;
     const { initialFormValues } = this.state;
@@ -159,7 +168,16 @@ class SendConstant extends Component {
           >
             <FormTextField name='fromAddress' placeholder='From Address' editable={false}  />
             <CheckBoxField name='isPrivacy' label='Is Privacy' onFieldChange={this.handleShouldGetFee} />
-            <FormTextField name='toAddress' placeholder='To Address' onFieldChange={this.handleShouldGetFee} />
+            <FormTextField
+              name='toAddress'
+              placeholder='To Address'
+              onFieldChange={this.handleShouldGetFee}
+              prependView={
+                <TouchableOpacity onPress={this.handleQrScanAddress}>
+                  <MaterialCommunityIcons name='qrcode-scan' size={FONT.SIZE.large} />
+                </TouchableOpacity>
+              }
+            />
             <FormTextField name='amount' placeholder='Amount' onFieldChange={this.handleShouldGetFee}/>
             <FormTextField name='fee' placeholder='Min Fee' />
             <FormSubmitButton title='SEND' style={styleSheet.submitBtn} />
