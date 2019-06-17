dca7b0eaa9a3747637ae503121e1e47112c39eb5
Unknown <t.hien@autonomous.nyc>
2019-06-17 17:48:06 +0700

    [refactor] add some cmd

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ import './shim.js';
 import {AppRegistry} from 'react-native';
 import App from '@src/App';
 import {name as appName} from './app.json';
-
+console.disableYellowBox = true;
 AppRegistry.registerComponent(appName, () => App);
