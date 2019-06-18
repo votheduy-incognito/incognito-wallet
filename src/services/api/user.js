b335ca3af1331f0a9a6fe4a5612e28a07c0a22b1
Keresh <vuong@autonomous.nyc>
2019-06-18 16:11:59 +0700

    Adding unlucky codes :(

--- src/services/api/user.js
+++ src/services/api/user.js
@@ -3,3 +3,7 @@ import http from '../http';
 export const subscribeEmail = email => http.post('/auth/subscribe', {
   Email: email,
 });
+
+export const getTokenFromEmail = email => http.post('/auth/token', {
+  Email: email,
+});
