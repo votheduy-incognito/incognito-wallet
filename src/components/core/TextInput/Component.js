3eb0cf5539129f3533d2c5284ba2df085c9c0164
Keresh <vuong@autonomous.nyc>
2019-07-24 18:14:00 +0700

    Update TextInput style

--- src/components/core/TextInput/Component.js
+++ src/components/core/TextInput/Component.js
@@ -1,19 +1,62 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-import { TextInput as RNComponent, View } from 'react-native';
+import React, { useState } from 'react';
+import { TextInput as RNComponent, View, Text } from 'react-native';
 import styleSheet from './style';
 
-const TextInput = ({ containerStyle, inputStyle, prependView, ...props }) => (
-  <View style={[styleSheet.container, containerStyle]}>
-    <RNComponent {...props} style={[styleSheet.input, inputStyle]} />
-    {prependView}
-  </View>
-);
+const TextInput = ({ containerStyle, inputStyle, prependView, label, onFocus, onBlur, ...props }) => {
+  const [focus, setFocus] = useState(false);
+
+  function handleFocus() {
+    setFocus(true);
+    if (typeof onFocus === 'function') {
+      onFocus.apply(this, arguments);
+    }
+  }
+
+  function handleBlur() {
+    setFocus(false);
+    if (typeof onBlur === 'function') {
+      onBlur.apply(this, arguments);
+    }
+  }
+
+  return (
+    <View style={styleSheet.container}>
+      {label && <Text style={[styleSheet.label, focus && styleSheet.labelFocus]}>{label}</Text>}
+      <View
+        style={[
+          styleSheet.row,
+          containerStyle,
+          focus && styleSheet.focus
+        ]}
+      >
+        <RNComponent
+          {...props}
+          style={[
+            styleSheet.input,
+            inputStyle,
+          ]}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+        {prependView}
+      </View>
+    </View>
+  );
+};
+
+TextInput.defaultProps = {
+  label: null,
+  containerStyle: null,
+  inputStyle: null,
+  prependView: null
+};
 
 TextInput.propTypes = {
-  containerStyle: PropTypes.objectOf(PropTypes.object),
-  inputStyle: PropTypes.objectOf(PropTypes.object),
-  prependView: PropTypes.element
+  containerStyle: PropTypes.object,
+  inputStyle: PropTypes.object,
+  prependView: PropTypes.element,
+  label: PropTypes.string,
 };
 
 export default TextInput;
--- src/components/core/TextInput/style.js
+++ src/components/core/TextInput/style.js
@@ -1,14 +1,36 @@
-import { THEME } from '@src/styles';
+import { THEME, DECOR, COLORS } from '@src/styles';
 import { StyleSheet } from 'react-native';
 
 const style = StyleSheet.create({
   container: {
+    flexDirection: 'column',
+    marginBottom: 5,
+  },
+  row: {
     ...THEME.textInput,
+    height: DECOR.inputHeight,
     alignItems: 'center',
     flexDirection: 'row'
   },
+  label: {
+    ...THEME.text.defaultTextStyle,
+    fontSize: 14,
+    color: COLORS.black,
+    marginBottom: 5,
+  },
+  labelFocus: {
+    color: COLORS.blue
+  },
   input: {
-    flex: 1
+    flex: 1,
+    height: DECOR.inputHeight,
+    paddingVertical: 3,
+    fontSize: 20,
+    color: COLORS.black,
+  },
+  focus: {
+    borderColor: COLORS.blue,
+    borderBottomWidth: DECOR.borderWidth + 0.5
   }
 });
 
