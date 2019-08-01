a870cfe30220cbbdd0775a4b0bcff80f6b63a798
Keresh <vuong@autonomous.nyc>
2019-08-01 14:55:11 +0700

    Add redux inputQR field

---
+++ src/components/core/reduxForm/fields/inputQR.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { TouchableOpacity, TextInput } from '@src/components/core';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { openQrScanner } from '@src/components/QrCodeScanner';
+import createField from './createField';
+
+const renderCustomField = ({ input, meta, ...props }) => {
+  const { onChange, onBlur, onFocus, value } = input;
+  return (
+    <TextInput
+      {...props}
+      onChangeText={(t) => onChange(t)}
+      onBlur={onBlur}
+      onFocus={onFocus}
+      defaultValue={value}
+      prependView={(
+        <TouchableOpacity
+          onPress={() => {
+            openQrScanner(data => {
+              onChange(data);
+            });
+          }}
+        >
+          <MaterialCommunityIcons name='qrcode-scan' size={20} />
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+const InputQRField = createField({
+  fieldName: 'InputQRField',
+  render: renderCustomField
+});
+
+
+export default InputQRField;
--- src/components/core/reduxForm/index.js
+++ src/components/core/reduxForm/index.js
@@ -1,3 +1,4 @@
 export { default as createForm } from './createForm';
 export { default as InputField } from './fields/input';
+export { default as InputQRField } from './fields/inputQR';
 export { default as validator } from './validator';
