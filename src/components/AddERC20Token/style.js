f73f211879498ff9ab1275a1167205e0252df525
Keresh <vuong@autonomous.nyc>
2019-08-02 14:56:57 +0700

    Add AddERC20Token component

---
+++ src/components/AddERC20Token/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+  },
+  mainContainer: {
+    flex: 1,
+  },
+  form: {
+  },
+  submitBtn: {
+    marginTop: 20,
+  },
+  feeText: {
+    textAlign: 'center',
+    marginVertical: 10,
+    fontSize: 14
+  },
+  input: {
+    marginBottom: 15
+  }
+});
--- src/services/api/token.js
+++ src/services/api/token.js
@@ -1,6 +1,32 @@
 import http from '@src/services/http';
 import PToken from '@src/models/pToken';
+import Erc20Token from '@src/models/erc20Token';
 
 export const getTokenList = () =>
   http.get('ptoken/list')
     .then(res => res.map(token => new PToken(token)));
+
+export const detectERC20Token = erc20Address => {
+  if (!erc20Address) throw new Error('Missing erc20Address to detect');
+  return http.post('eta/detect-erc20', {
+    Address: erc20Address
+  })
+    .then(res => new Erc20Token(res));
+};
+
+export const addERC20Token = ({ symbol, name, contractId, decimals }) => {
+  const parseDecimals = Number(decimals);
+
+  if (!symbol) throw new Error('Missing symbol');
+  if (!name) throw new Error('Missing name');
+  if (!contractId) throw new Error('Missing contractId');
+  if (!Number.isInteger(parseDecimals)) throw new Error('Invalid decimals');
+
+  return http.post('ptoken/add', {
+    Symbol: symbol,
+    Name: name,
+    ContractID: contractId,
+    Decimals: parseDecimals
+  })
+    .then(res => new PToken(res));
+};
