b03c08db937c239dbbeab142d3b31c0cfdf1dc9f
Keresh <vuong@autonomous.nyc>
2019-08-20 12:00:45 +0700

    Add SelfStaking

--- src/components/SelfStaking/SelfStaking.js
+++ src/components/SelfStaking/SelfStaking.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button, ScrollView, View, Text } from '@src/components/core';
+import { Button, ScrollView, View, Text, Toast } from '@src/components/core';
 import EstimateFee from '@src/components/EstimateFee';
 import StakeValidatorTypeSelector from '@src/components/StakeValidatorTypeSelector';
 import tokenData from '@src/constants/tokenData';
 import { CONSTANT_COMMONS } from '@src/constants';
+import LoadingTx from '@src/components/LoadingTx';
 import formatUtil from '@src/utils/format';
 import styles from './style';
 
@@ -12,6 +13,7 @@ class SelfStaking extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isStaking: false,
       finalFee: null,
       feeUnit: null,
       amount:  null,
@@ -27,8 +29,31 @@ class SelfStaking extends Component {
     this.setState({ stakeTypeId: id, amount });
   }
 
+  handleStake = async () => {
+    try {
+      this.setState({
+        isStaking: true
+      });
+
+      const { onStaking } = this.props;
+      const { stakeTypeId, finalFee } = this.props;
+      if (typeof onStaking === 'function') {
+        await onStaking({
+          stakeType: stakeTypeId,
+          fee: finalFee
+        });
+      }
+    } catch {
+      Toast.showError('Stake failed, please try again');
+    } finally {
+      this.setState({
+        isStaking: false
+      });
+    }
+  }
+
   render() {
-    const { amount, finalFee, feeUnit, stakeTypeId } = this.state;
+    const { amount, finalFee, feeUnit, stakeTypeId, isStaking } = this.state;
     const { selectedPrivacy } = this.props;
     const toAddress = selectedPrivacy?.paymentAddress;
 
@@ -50,10 +75,11 @@ class SelfStaking extends Component {
             style={styles.estFee}
           />
           <Text style={styles.feeText}>
-            Fee: {formatUtil.amount(finalFee, feeUnit === tokenData.SYMBOL.MAIN_CRYPTO_CURRENCY ? CONSTANT_COMMONS.DECIMALS.MAIN_CRYPTO_CURRENCY : null)} {feeUnit}
+            You&apos;ll pay: {formatUtil.amount(finalFee, feeUnit === tokenData.SYMBOL.MAIN_CRYPTO_CURRENCY ? CONSTANT_COMMONS.DECIMALS.MAIN_CRYPTO_CURRENCY : null)} {feeUnit}
           </Text>
-          <Button title='Stake' style={styles.stakeButton} />
+          <Button title='Stake' style={styles.stakeButton} onPress={this.handleStake} />
         </ScrollView>
+        { isStaking && <LoadingTx /> }
       </View>
     );
   }
--- src/components/SelfStaking/index.js
+++ src/components/SelfStaking/index.js
@@ -2,34 +2,64 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { selectedPrivacy } from '@src/redux/selectors/selectedPrivacy';
+import { getAccountByName } from '@src/redux/selectors/account';
+import accountService from '@src/services/wallet/accountService';
+import { CONSTANT_COMMONS } from '@src/constants';
 import SelfStaking from './SelfStaking';
 
 export class SelfStakingContainer extends Component {
-  static propTypes = {
-    selectedPrivacy: PropTypes.object
+  handleStaking = async ({ stakeType, fee }) => {
+    try {
+      const { wallet, accountName, getAccountByName } = this.props;
+      const account = getAccountByName(accountName);
+      const param = {
+        type: Number(stakeType),
+        burningAddress: CONSTANT_COMMONS.STAKING_ADDRESS
+      };
+      const candidatePaymentAddress = null;
+      const isRewardFunder = false; // reward will be paid for miners
+
+      const rs = await accountService.staking(param, fee, candidatePaymentAddress, isRewardFunder, account, wallet);
+
+      return rs;
+    } catch (e) {
+      throw e;
+    }
   }
 
-  static defaultProps = {
-    selectedPrivacy: null
-  };
-
   render() {
-    const { selectedPrivacy } = this.props;
+    const { selectedPrivacy, accountName, getAccountByName } = this.props;
 
-    if (!selectedPrivacy) return null;
+    if (!selectedPrivacy || !getAccountByName(accountName)) return null;
 
     return (
-      <SelfStaking {...this.props} />
+      <SelfStaking
+        {...this.props}
+        onStaking={this.handleStaking}
+      />
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  selectedPrivacy: selectedPrivacy(state)
+  selectedPrivacy: selectedPrivacy(state),
+  wallet: state?.wallet,
+  getAccountByName: getAccountByName(state),
 });
 
 const mapDispatchToProps = {
   
+};
+
+
+SelfStakingContainer.defaultProps = {
+  accountName: null,
+  selectedPrivacy: null
+};
+
+SelfStakingContainer.propTypes = {
+  accountName: PropTypes.string,
+  selectedPrivacy: PropTypes.object
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SelfStakingContainer);
