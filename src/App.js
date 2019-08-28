a1d1c0c95e6837273d1922bd77672c3f7b4ee506
Keresh <vuong@autonomous.nyc>
2019-08-28 15:08:26 +0700

    Implement AppScreen to Root App & Modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 // import 'intl';
 // import 'intl/locale-data/jsonp/en';
 import QrScanner from '@src/components/QrCodeScanner';
+import AppScreen from '@src/components/AppScreen';
 import configureStore from '@src/redux/store';
 import AppContainer from '@src/router';
 import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { initFirebaseNotification, onFirebaseMessage } from '@src/services/firebase';
-import { StyleSheet, Platform, StatusBar } from 'react-native';
-import { SafeAreaView } from 'react-navigation';
-import DeviceInfo from 'react-native-device-info';
+import { StatusBar } from 'react-native';
 import { THEME } from './styles';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: THEME.header.backgroundColor,
-    paddingTop: (Platform.OS === 'android' && DeviceInfo.hasNotch()) ? StatusBar.currentHeight : 0
-  }
-});
 
 StatusBar.setBackgroundColor(THEME.header.backgroundColor);
 
@@ -39,10 +30,10 @@ const App = () => {
 
   return (
     <Provider store={store}>
-      <SafeAreaView style={styles.container} forceInset={{ bottom: 'never' }}>
+      <AppScreen>
         <AppContainer />
         <QrScanner />
-      </SafeAreaView>
+      </AppScreen>
     </Provider>
   );
 };
