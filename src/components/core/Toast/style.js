48541d730e683220ef40992c0a81c0b2a1e94b03
Keresh <vuong@autonomous.nyc>
2019-08-29 17:06:03 +0700

    Add new Toast component

---
+++ src/components/core/Toast/style.js
@@ -0,0 +1,42 @@
+import { SPACING, COLORS, UTILS, DECOR } from '@src/styles';
+import { StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    position: 'absolute',
+    alignSelf: 'center',
+    bottom: 150,
+    padding: 10,
+    backgroundColor: COLORS.toastBackgroundDefault,
+    maxHeight: 300,
+    margin: 30,
+    borderWidth: DECOR.borderWidth,
+    borderRadius: 4,
+  },
+  message: {
+    color: COLORS.white,
+    marginHorizontal: 10
+  },
+  warningMessage: {
+    color: '#47412C'
+  },
+  errorContainer: {
+    backgroundColor: COLORS.toastBackgroundError,
+    borderColor: '#FF580F',
+  },
+  warningContainer: {
+    backgroundColor: COLORS.toastBackgroundWarning,
+    borderColor: '#FECD01',
+  },
+  infoContainer: {
+    backgroundColor: COLORS.toastBackgroundDefault,
+    borderColor: '#8C9C9D',
+  },
+  successContainer: {
+    backgroundColor: COLORS.toastBackgroundSuccess,
+    borderColor: '#019788',
+  },
+});
+
+export default style;
