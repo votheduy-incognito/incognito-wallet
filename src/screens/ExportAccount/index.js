efdbd65fa94c7e85db2bd8de8f62b1fbe25599a4
Keresh <vuong@autonomous.nyc>
2019-09-07 13:53:19 +0700

    Fixed ExportAccount exports wrong account

--- src/screens/ExportAccount/index.js
+++ src/screens/ExportAccount/index.js
@@ -1,12 +1,10 @@
 import LoadingContainer from '@src/components/LoadingContainer';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux';
-import { accountSeleclor } from '@src/redux/selectors';
 import ExportAccount from './ExportAccount';
 
-const ExportAccountContainer = ({ account, navigation }) => {
-  account = account || navigation?.getParam('account');
+const ExportAccountContainer = ({ navigation }) => {
+  const account = navigation?.getParam('account');
 
   if (!account) {
     return <LoadingContainer />;
@@ -17,11 +15,6 @@ const ExportAccountContainer = ({ account, navigation }) => {
 
 ExportAccountContainer.propTypes = {
   navigation: PropTypes.object.isRequired,
-  account: PropTypes.object.isRequired
 };
 
-const mapState = state => ({
-  account: accountSeleclor.defaultAccount(state)
-});
-
-export default connect(mapState)(ExportAccountContainer);
+export default ExportAccountContainer;
