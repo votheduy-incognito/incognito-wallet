85e4cfbd15b175fdf5b20e8b573161c857ed6cd0
Unknown <t.hien@autonomous.nyc>
2019-10-16 16:47:28 +0700

    [MINER] config 0.60

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 // import 'intl';
 // import 'intl/locale-data/jsonp/en';
+import 'react-native-console-time-polyfill';
 import AppScreen from '@src/components/AppScreen';
 import { Toast } from '@src/components/core';
 import QrScanner from '@src/components/QrCodeScanner';
 import configureStore from '@src/redux/store';
 import AppContainer from '@src/router';
+import { CustomError, ErrorCode, ExHandler } from '@src/services/exception';
 import { initFirebaseNotification } from '@src/services/firebase';
 import React, { PureComponent } from 'react';
 import { StatusBar } from 'react-native';
-import 'react-native-console-time-polyfill';
-import { ExHandler, CustomError, ErrorCode } from '@src/services/exception';
+
 import { Provider } from 'react-redux';
 import { THEME } from './styles';
-
-
 
 StatusBar.setBackgroundColor(THEME.statusBar.backgroundColor);
 
