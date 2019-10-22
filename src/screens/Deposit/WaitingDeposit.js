93ee9b8abb7103354a55e5967f4d30bb9dd5f7e8
Keresh <vuong@autonomous.nyc>
2019-10-22 15:06:39 +0700

    Update WaitingDeposit

--- src/screens/Deposit/WaitingDeposit.js
+++ src/screens/Deposit/WaitingDeposit.js
@@ -4,11 +4,11 @@ import { Text, View } from '@src/components/core';
 import QrCodeAddress from '@src/components/QrCodeAddress';
 import { waitingDepositStyle } from './style';
 
-const WaitingDeposit = ({ selectedPrivacy, depositAddress, amount }) => {
+const WaitingDeposit = ({ selectedPrivacy, depositAddress }) => {
   return (
     <View style={waitingDepositStyle.container}>
       <View style={waitingDepositStyle.textContainer}>
-        <Text style={[waitingDepositStyle.text, waitingDepositStyle.textHighlight]}>Please send {amount} {selectedPrivacy?.externalSymbol} to this address within the next 60 minutes.</Text>
+        <Text style={[waitingDepositStyle.text, waitingDepositStyle.textHighlight]}>Please send {selectedPrivacy?.externalSymbol} to this address within the next 60 minutes.</Text>
       </View>
       <QrCodeAddress data={depositAddress} />
       <Text style={[waitingDepositStyle.text, { marginTop: 50 }]}>You can close this screen anytime. You&apos;ll receive a notification once your wallet balance has been updated.</Text>
@@ -17,14 +17,9 @@ const WaitingDeposit = ({ selectedPrivacy, depositAddress, amount }) => {
   );
 };
 
-WaitingDeposit.defaultProps = {
-  amount: 0
-};
-
 WaitingDeposit.propTypes = {
   selectedPrivacy: PropTypes.object.isRequired,
   depositAddress: PropTypes.string.isRequired,
-  amount: PropTypes.oneOfType([ PropTypes.string, PropTypes.number])
 };
 
 export default WaitingDeposit;
