5bca68b6bc6aa21c9f5f4bbed80503b2b478560d
Unknown <t.hien@autonomous.nyc>
2019-10-25 15:22:55 +0700

    Auto stash before rebase of "develop"

--- src/components/core/TextInput/InputExtension.js
+++ src/components/core/TextInput/InputExtension.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Input} from 'react-native-elements';
 import { TextInput } from '..';
+// import {TextInput as RNTextInput} from 'react-native';
+
+// const TextInput = React.memo((props) => {
+//   return (
+//     <RNTextInput clearButtonMode='while-editing' allowFontScaling={false} {...props} />
+//   );
+// });
 
 const InputExtension = React.memo(props=>{
   const labelProps = {allowFontScaling:false,...(props?.labelProps??{})};
   const errorProps = {allowFontScaling:false,...(props?.errorProps??{})};
   return <Input inputComponent={TextInput} {...props} labelProps={labelProps} errorProps={errorProps} />;
 });
+
 
 export default InputExtension;
