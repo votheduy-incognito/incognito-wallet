27f8758082e0289c1174c6ff90b1ab240053eccf
Keresh <vuong@autonomous.nyc>
2019-11-11 17:17:09 +0700

    Update regex patterns for tokenName, accountName

--- src/components/core/reduxForm/validator.js
+++ src/components/core/reduxForm/validator.js
@@ -107,9 +107,9 @@ const combinedTOMOAddress = [required(), tomoAddress()];
 const combinedBTCAddress = [required(), btcAddress()];
 const combinedBNBAddress = [required(), bnbAddress()];
 const combinedUnknownAddress = [required(), minLength(15)];
-const combinedTokenName = [required(), minLength(3), maxLength(50), regexp(/^[a-zA-Z]((\w+)?(( |-){1}\w+)?)+$/i, { message: 'Please use a valid token name (Ex: "My Token, Token-1,..").' })];
+const combinedTokenName = [required(), minLength(3), maxLength(50), regexp(/\w+$/i, { message: 'Please use a valid token name (Ex: "My Token, Token-1,..").' })];
 const combinedTokenSymbol = [required(), minLength(2), maxLength(10), regexp(/^[A-Z]+$/, { message: 'Please use a valid token symbol (Ex: "SYM").' })];
-const combinedAccountName = [required(), minLength(1), maxLength(50), regexp(/^[a-zA-Z]((\w+)?(( |-){1}\w+)?)+$/i, { message: 'Please use a valid account name (Ex: "Cat, Account-1,..").' })];
+const combinedAccountName = [required(), minLength(1), maxLength(50), regexp(/\w+$/i, { message: 'Please use a valid account name (Ex: "Cat, Account-1,..").' })];
 
 export default {
   required,
