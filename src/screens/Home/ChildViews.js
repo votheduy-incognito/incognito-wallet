cfd6f3709cc606e1a2c3a4d5a2877837cfde4f62
Keresh <vuong@autonomous.nyc>
2019-11-26 17:53:52 +0700

    Update airdrop UI

--- src/screens/Home/ChildViews.js
+++ src/screens/Home/ChildViews.js
@@ -4,17 +4,18 @@ import Dialog, { DialogContent } from 'react-native-popup-dialog';
 import { Text, ButtonExtension, Image, ScrollView } from '@src/components/core';
 import { scaleInApp } from '@src/styles/TextStyle';
 import Icons from 'react-native-vector-icons/Feather';
-import airdropImg from '@src/assets/images/airdrop.png';
+// import airdropImg from '@src/assets/images/airdrop.png';
 import { COLORS, FONT } from '@src/styles';
 
 const style = StyleSheet.create({
   container: {
-    paddingLeft: 0,
-    paddingRight: 0
+    flex: 1,
+    paddingVertical: scaleInApp(25),
+    paddingLeft: scaleInApp(25),
+    paddingRight: scaleInApp(25),
   },
   content: {
-    padding: scaleInApp(31),
-    paddingBottom: 150,
+    flex: 1,
   },
   titleText: {
     fontSize: scaleInApp(18),
@@ -30,7 +31,7 @@ const style = StyleSheet.create({
   item: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginVertical: 8
+    marginVertical: 8,
   },
   itemIcon: {
     marginRight: scaleInApp(15)
@@ -40,13 +41,14 @@ const style = StyleSheet.create({
     color: COLORS.black,
     letterSpacing: 0,
     lineHeight: scaleInApp(23),
+    marginRight: 50,
   },
   descText: {
     fontSize: scaleInApp(16),
     color: COLORS.lightGrey9,
     letterSpacing: 0,
     lineHeight: scaleInApp(23),
-    marginBottom: scaleInApp(40)
+    marginBottom: scaleInApp(10),
   },
   buttonText: {
     fontSize: scaleInApp(16),
@@ -64,19 +66,18 @@ const DialogUpgradeToMainnet = React.memo(({ isVisible = true ,onButtonClick })=
   return (
     <Dialog
       width={0.9}
-      height={0.65}
+      height={0.7}
       visible={isVisible}
       onTouchOutside={() => {
       }}
     > 
       <DialogContent style={style.container}>
-        <Image style={style.image} resizeMode="cover" source={airdropImg} />
-        <ScrollView>
+        <ScrollView contentContainerStyle={{ minHeight: '100%' }}>
           <View style={style.content}>
             <Text style={style.titleText}>Welcome to Incognito! Here’s a little something to get you started.</Text>
             <View style={style.items}>
               {
-                ['1 PRV is on its way to your default account (Account 0).', '1.1 PRV is on its way to your trading account (pDEX) ']
+                ['1 PRV is on its way to your default account', '1.1 PRV is on its way to your trading account (pDEX)']
                   .map((text) => (
                     <View key={text} style={style.item}>
                       <Icons name='check' color={COLORS.green} style={style.itemIcon} size={28} />
@@ -85,16 +86,16 @@ const DialogUpgradeToMainnet = React.memo(({ isVisible = true ,onButtonClick })=
                   ))
               }
             </View>
-
-            <Text style={style.descText}>This will cover fees for thousands of transactions. We hope you enjoy Incognito!</Text>
-            <ButtonExtension
-              titleStyle={style.buttonText}
-              buttonStyle={style.button}
-              onPress={onButtonClick}
-              title='Go incognito'
-            />
+            <Text style={style.descText}>This will cover fees for thousands of transactions. It may take a while to reach your account. Please be patient.</Text>
+            <Text style={style.descText}>We hope you enjoy Incognito!</Text>
           </View>
         </ScrollView>
+        <ButtonExtension
+          titleStyle={style.buttonText}
+          buttonStyle={style.button}
+          onPress={onButtonClick}
+          title='Go incognito'
+        />
       </DialogContent>
 
     </Dialog>
