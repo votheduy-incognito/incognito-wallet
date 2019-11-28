37e631c39dff4e1fa49f802f27e278a72bec37a9
Keresh <vuong@autonomous.nyc>
2019-11-28 14:10:10 +0700

    Add testnet token ID for new deposit flow

--- src/screens/Deposit/index.js
+++ src/screens/Deposit/index.js
@@ -76,8 +76,11 @@ class DepositContainer extends Component {
     return [
       '716fd1009e2a1669caacc36891e707bfdf02590f96ebd897548e8963c95ebac0',
       '1ff2da446abfebea3ba30385e2ca99b0f0bbeda5c6371f4c23c939672b429a42',
-      '1a32a6beaf79d435ff3b7fde2e166c2d67f61a64d4e482bb6633668516c1531f',
-      '9e1142557e63fd20dee7f3c9524ffe0aa41198c494aa8d36447d12e85f0ddce7'
+      'd240c61c6066fed0535df9302f1be9f5c9728ef6d01ce88d525c4f6ff9d65a56',
+      '9e1142557e63fd20dee7f3c9524ffe0aa41198c494aa8d36447d12e85f0ddce7',
+
+      // testnet
+      '4946b16a08a9d4afbdf416edf52ef15073db0fc4a63e78eb9de80f94f6c0852a'
     ].includes(tokenId);
   }
 
