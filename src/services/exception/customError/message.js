65d7b27d04677b0afafabbdb801e9a77473816e4
Keresh <vuong@autonomous.nyc>
2019-12-02 16:11:58 +0700

    Update copy

--- src/services/exception/customError/message.js
+++ src/services/exception/customError/message.js
@@ -11,7 +11,7 @@ export default {
   [CODE.withdraw_gen_withdraw_address_failed]: 'Can not get withdraw address, please try again.',
   [CODE.getStarted_can_not_create_wallet_on_existed]: 'Can not create new wallet on existing wallet.',
   [CODE.wallet_can_not_create_new_wallet]: 'Sorry, we can not create new wallet, close and reopen the app can fix it.',
-  [CODE.wallet_can_not_load_existed_wallet]: 'Can not open your wallet. Please re-install the application and try again.',
+  [CODE.wallet_can_not_load_existed_wallet]: 'Something’s not right. Please re-install this app and try again.',
   [CODE.createAccount_failed]: 'Account was not created! Please try again.',
   [CODE.createAccount_existed_name]: 'You already have an account with this name. Please try another.',
   [CODE.home_load_following_token_failed]: 'Is your list token not show? Pull to reload your the list.',
