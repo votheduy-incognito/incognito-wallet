aec464cc7ec5118a55497406cc883784a5378397
0xSiris <siris@incognito.org>
2020-01-08 16:16:19 +0700

    Fix pin code

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -68,5 +68,16 @@ export default {
     console.debug('CONVERT', originalText, text);
 
     return _.toNumber(text);
+  },
+
+  toHash(text) {
+    let hash = 0, i, chr;
+    if (text.length === 0) return '';
+    for (i = 0; i < text.length; i++) {
+      chr   = text.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash.toString();
   }
 };
