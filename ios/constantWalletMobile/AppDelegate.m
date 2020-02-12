43be61b54580666d5b642617b5f104fd138cbd51
0xSiris <siris@incognito.org>
2020-02-12 09:47:42 +0700

    APP-69: Track app install

--- ios/constantWalletMobile/AppDelegate.m
+++ ios/constantWalletMobile/AppDelegate.m
@@ -18,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [[AppsFlyerTracker sharedTracker] trackAppLaunch];
   [FIRApp configure];
   [RNFirebaseNotifications configure];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
@@ -32,6 +33,14 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  /** APPSFLYER INIT **/
+  [AppsFlyerTracker sharedTracker].appsFlyerDevKey = @"b3jPTau376R98i6xKeFShJ";
+  [AppsFlyerTracker sharedTracker].appleAppID = @"1475631606";
+  [AppsFlyerTracker sharedTracker].delegate = self;
+  /* Set isDebug to true to see AppsFlyer debug logs */
+  [AppsFlyerTracker sharedTracker].isDebug = true;
+    
   return YES;
 }
 
@@ -47,6 +56,45 @@
 
 - (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
   [[RNFirebaseNotifications instance] didReceiveLocalNotification:notification];
+}
+
+// Deep linking
+
+// Open Universal Links
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id> * _Nullable))restorationHandler {
+    [[AppsFlyerTracker sharedTracker] continueUserActivity:userActivity restorationHandler:restorationHandler];
+    return YES;
+}
+
+// Report Push Notification attribution data for re-engagements
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    [[AppsFlyerTracker sharedTracker] handlePushNotification:userInfo];
+}
+
+
+// AppsFlyerTracker implementation
+
+//Handle Conversion Data (Deferred Deep Link)
+-(void)onConversionDataSuccess:(NSDictionary*) installData {
+    id status = [installData objectForKey:@"af_status"];
+    if([status isEqualToString:@"Non-organic"]) {
+        id sourceID = [installData objectForKey:@"media_source"];
+        id campaign = [installData objectForKey:@"campaign"];
+        NSLog(@"This is a none organic install. Media source: %@  Campaign: %@",sourceID,campaign);
+    } else if([status isEqualToString:@"Organic"]) {
+        NSLog(@"This is an organic install.");
+    }
+}
+-(void)onConversionDataFail:(NSError *) error {
+  NSLog(@"%@",error);
+}
+
+//Handle Direct Deep Link
+- (void) onAppOpenAttribution:(NSDictionary*) attributionData {
+  NSLog(@"%@",attributionData);
+}
+- (void) onAppOpenAttributionFailure:(NSError *)error {
+  NSLog(@"%@",error);
 }
 
 @end
