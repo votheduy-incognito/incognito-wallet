a06b53e0c4f9b5d56d3c2c87b27c975714f24b75
0xSiris <siris@incognito.org>
2020-04-12 16:17:18 +0700

    Config CI for IOS

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "
+PASSWORD_SECRET_KEY=$PASSWORD_SECRET_KEY
+PASSPHRASE_WALLET_DEFAULT=$PASSPHRASE_WALLET_DEFAULT
+API_MINER_URL=$API_MINER_URL
+PASS_HOSPOT=$PASS_HOSPOT
+" > .env
+
+#IOS_BUILD_CONFIGURATION=Test
+
+if [[ ! -z "${IOS_BUILD_CONFIGURATION}" ]]; then
+  FILE=ios/Incognito.xcworkspace/xcshareddata/xcschemes/Incognito.xcscheme
+  BACKUP_FILE=Incognito.xcscheme.bak
+  sed '75 s/buildConfiguration = ".*"/buildConfiguration = "'$IOS_BUILD_CONFIGURATION'"/' $FILE > $BACKUP_FILE
+  mv $BACKUP_FILE $FILE
+fi
