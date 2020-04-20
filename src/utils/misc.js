8e5d4e7bc67b56ab68115fcbf806b44e1b98b0cb
uranashel <tan@incognito.org>
2020-04-20 16:29:13 +0700

    Combine both id

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -9,8 +9,5 @@ export const detectToken = {
 };
 
 export const generateTestId = (id) => {
-  if (isIOS()) {
-    return { testID: id };
-  }
-  return { accessibilityLabel: id };
+  return { accessibilityLabel: id, testID: id };
 };
