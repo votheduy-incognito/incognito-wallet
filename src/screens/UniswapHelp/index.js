d6ab0323f0b152dc98d2682f2eaa97a80c01f9a3
0xSiris <siris@incognito.org>
2020-04-20 15:21:42 +0700

    Update text in FAQs

--- src/screens/UniswapHelp/index.js
+++ src/screens/UniswapHelp/index.js
@@ -5,24 +5,24 @@ import styles from './style';
 
 const questions = [
   {
-    text: 'How does pUniswap work?',
-    answer: 'pUniswap allows you to trade using Uniswap liquidity pools. The only difference is that everything is anonymous.',
+    text: 'How does pKyber work?',
+    answer: 'pKyber allows you to trade using Kyber liquidity pools. The only difference is that everything is anonymous.',
   },
   {
     text: 'Is this version on the mainnet?',
-    answer: 'pUniswap is currently on the testnet. The mainnet launch is scheduled for mid May 2020.',
+    answer: 'pKyber is currently on the testnet. The mainnet launch is scheduled for mid May 2020.',
   },
   {
     text: 'How do I get started?',
-    answer: 'Testnet pETH (privacy ETH)  will be released to you the first time you open pUniswap. This may take a couple of minutes. Once you receive them, you’re good to go! Trade your pETH for any currency listed.',
+    answer: 'Testnet pETH (privacy ETH)  will be released to you the first time you open pKyber. This may take a couple of minutes. Once you receive them, you’re good to go! Trade your pETH for any currency listed.',
   },
   {
     text: 'How do I withdraw?',
-    answer: 'pUniswap testnet tokens cannot be withdrawn to another account.'
+    answer: 'pKyber testnet tokens cannot be withdrawn to another account.'
   },
   {
-    text: 'How is pUniswap different from the Trade feature in the wallet?',
-    answer: 'pUniswap taps into Uniswap liquidity. Essentially – you’re trading on Uniswap, just that everything is private.  Trade gives you access to liquidity pools on the Incognito privacy DEX.',
+    text: 'How is pKyber different from the Trade feature in the wallet?',
+    answer: 'pKyber taps into Kyber liquidity. Essentially – you’re trading on Kyber, just that everything is private.  Trade gives you access to liquidity pools on the Incognito privacy DEX.',
   },
 ];
 
