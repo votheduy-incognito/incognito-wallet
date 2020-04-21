a41e34b81cb504b6fcf09a52bc7a5cbfbd357343
uranashel <tan@incognito.org>
2020-04-21 15:06:46 +0700

    Change text for warning withdraw address erc20/eth

--- src/screens/SendCrypto/SendOut/Withdraw.js
+++ src/screens/SendCrypto/SendOut/Withdraw.js
@@ -414,7 +414,7 @@ class Withdraw extends React.Component {
                 />
                 {(isErc20Token || externalSymbol === CONSTANT_COMMONS.CRYPTO_SYMBOL.ETH) && (
                   <Text style={[style.warning, shouldBlockETHWrongAddress ? { color: COLORS.red } : {}]}>
-                    Please withdraw to wallet addresses only.
+                    Please withdraw to a wallet address, not a smart contract address.
                   </Text>
                 )}
                 <Field
