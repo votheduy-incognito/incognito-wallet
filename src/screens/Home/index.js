9fc198fe24e032360820fd314bd69b8d78fd9043
uranashel <tan@incognito.org>
2020-04-22 19:37:08 +0700

    Update touchable without feedback

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, Dimensions, PixelRatio, Platform, SafeAreaView } from 'react-native';
+import { ScrollView, Dimensions, PixelRatio, Platform, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
 import LinkingService from '@src/services/linking';
 import AppUpdater from '@components/AppUpdater/index';
 import { isIOS } from '@utils/platform';
@@ -206,37 +206,35 @@ const Home = ({ navigation }) => {
   }, []);
 
   return (
-    <TouchableOpacity style={{ flex: 1 }} onPress={closeTooltip}>
-      <SafeAreaView>
-        <ScrollView contentContainerStyle={{height: '100%'}}>
-          <View>
-            <Text numberOfLines={3} multiLine style={styles.titleHeader}>{'Incognito mode \nFor your crypto'}
-            </Text>
-            <View style={styles.btnContainer}>
-              {buttons.map(item => (
-                <View style={styles.btn} key={item.title}>
-                  {item === pStakeItem &&
-                    viewUniswap !== tooltipType && (
-                    <Tooltip
-                      title="New"
-                      desc="Join a PRV staking pool. Get a 57% annual return. Interest paid every second."
-                    />
-                  )}
-                  <IconTextButton
-                    image={item.image}
-                    title={item.title}
-                    disabled={isDisabled(item)}
-                    onPress={
-                      item.onPress || (() => goToScreen(item.route, item.params))
-                    }
+    <TouchableWithoutFeedback style={{ flex: 1 }} onPress={closeTooltip}>
+      <ScrollView>
+        <View>
+          <Text numberOfLines={3} multiLine style={styles.titleHeader}>{'Incognito mode \nFor your crypto'}
+          </Text>
+          <View style={styles.btnContainer}>
+            {buttons.map(item => (
+              <View style={styles.btn} key={item.title}>
+                {item === pStakeItem &&
+                  viewUniswap !== tooltipType && (
+                  <Tooltip
+                    title="New"
+                    desc="Join a PRV staking pool. Get a 57% annual return. Interest paid every second."
                   />
-                </View>
-              ))}
-            </View>
+                )}
+                <IconTextButton
+                  image={item.image}
+                  title={item.title}
+                  disabled={isDisabled(item)}
+                  onPress={
+                    item.onPress || (() => goToScreen(item.route, item.params))
+                  }
+                />
+              </View>
+            ))}
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </TouchableWithoutFeedback>
   );
 };
 
