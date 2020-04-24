c202ee2bc73bd817fcda28e933b133879459098b
uranashel <tan@incognito.org>
2020-04-24 20:05:03 +0700

    Update modal for reporting txoutchain

--- src/screens/TxHistoryDetail/index.js
+++ src/screens/TxHistoryDetail/index.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import {
+  Dimensions,
+  Modal,
+  View, Text,
+  TextInput,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import LoadingContainer from '@src/components/LoadingContainer';
 import { retryExpiredDeposit } from '@src/services/api/history';
 import { ExHandler } from '@src/services/exception';
-import { Toast } from '@src/components/core';
+import { Toast, Button } from '@src/components/core';
+import LogManager from '@src/services/LogManager';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { FONT } from '@src/styles';
+import styles from './styles';
 import TxHistoryDetail from './TxHistoryDetail';
 
+const { width, height } = Dimensions.get('window');
 class TxHistoryDetailContainer extends Component {
   state = {
     data: null,
+    shouldShowTxModal: false,
+    txOutchain: '',
   };
 
   componentDidMount() {
@@ -30,12 +43,17 @@ class TxHistoryDetailContainer extends Component {
   retryExpiredDeposit = async (data) => {
     try {
       if (data) {
+        const { txOutchain } = this.state;
         const { decentralized } = data;
-        await retryExpiredDeposit(data);
+        if (txOutchain === '') {
+          return;
+        }
+        let newData = { ...data.history, TxOutchain: txOutchain };
+        await retryExpiredDeposit(newData);
 
         const decentralizedMSg = 'Your request has been sent, we will process it soon. The history status will be not changed';
         const centralizedMSg = 'Your request has been sent, we will process it soon. The history status will be updated';
-        
+
         Toast.showInfo(decentralized ? decentralizedMSg : centralizedMSg);
         this.goBack();
       }
@@ -43,7 +61,37 @@ class TxHistoryDetailContainer extends Component {
       new ExHandler(e, 'Sorry, we can not process this expired deposit request. Please try again.').showErrorToast();
     }
   }
-
+  renderModalTXOutchain = () => {
+    const { shouldShowTxModal } = this.state;
+    return (
+      <Modal
+        transparent
+        animationType="none"
+        visible={shouldShowTxModal}
+        onRequestClose={() => {
+          this.setState({ shouldShowTxModal: false });
+        }}
+      >
+        <TouchableWithoutFeedback onPress={() => this.setState({ shouldShowTxModal: false })}>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.titleModal}>{'If you\'ve already sent funds to the deposit address, just enter your transaction ID here. Your balance will update within 24 hours.'}</Text>
+              <TextInput onChangeText={text => this.setState({ txOutchain: text })} returnKeyType="done" placeholder="TransactionID" numberOfLines={1} style={styles.txField} />
+              <Button
+                style={styles.submitBTN}
+                title='Submit'
+                onPress={() => {
+                  const { data } = this.state;
+                  this.setState({ shouldShowTxModal: false });
+                  this.retryExpiredDeposit(data);
+                }}
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    );
+  }
   render() {
     const { data } = this.state;
 
@@ -51,7 +99,12 @@ class TxHistoryDetailContainer extends Component {
       return <LoadingContainer />;
     }
 
-    return <TxHistoryDetail {...this.props} data={data} onRetryExpiredDeposit={this.retryExpiredDeposit} />;
+    return (
+      <View>
+        <TxHistoryDetail {...this.props} data={data} onRetryExpiredDeposit={() => this.setState({ shouldShowTxModal: true })} />
+        {this.renderModalTXOutchain()}
+      </View>
+    );
   }
 }
 
