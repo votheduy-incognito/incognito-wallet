b1bff23ac99b300eea7e996f51321c3a775d0368
0xSiris <siris@incognito.org>
2020-05-13 10:17:30 +0700

    Edit release

--- src/components/EstimateFee/EstimateFee.utils.js
+++ src/components/EstimateFee/EstimateFee.utils.js
@@ -1,3 +1,3 @@
-export const DEFAULT_FEE_PER_KB = 0.0001 * 1e9; // in nano
+export const DEFAULT_FEE_PER_KB = 0.000000001 * 1e9; // in nano
 export const MAX_TX_SIZE = 100;
 export const MAX_FEE_PER_TX = DEFAULT_FEE_PER_KB * MAX_TX_SIZE;
