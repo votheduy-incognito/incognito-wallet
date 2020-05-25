5b7c8af4b7f55e103f1a74456bde8eecc1182293
jayce <phathoang@incognito.org>
2020-05-25 11:50:59 +0700

    New Wallet UI
    + Notification 'deposit-update' not working

--- src/screens/Home/Home.enhance.js
+++ src/screens/Home/Home.enhance.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ErrorBoundary from '@src/components/ErrorBoundary';
-import {compose} from 'recompose';
-import {withNavigation} from 'react-navigation';
+import { compose } from 'recompose';
+import { withNavigation } from 'react-navigation';
 import Modal from '@src/components/Modal';
+import withFCM from '@src/screens/Notification/Notification.withFCM';
 
 const enhance = WrappedComp => props => {
   return (
@@ -15,5 +16,6 @@ const enhance = WrappedComp => props => {
 
 export default compose(
   withNavigation,
+  withFCM,
   enhance,
 );
