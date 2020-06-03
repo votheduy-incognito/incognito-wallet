b6c3efafd57d3858ef69a5fe6e0f9b5d7211ab49
uranashel44 <tan@incognito.org>
2020-06-03 16:01:47 +0700

    Update caching community node

--- src/screens/Community/index.js
+++ src/screens/Community/index.js
@@ -1,28 +1,47 @@
 import React, { useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withNavigationFocus } from 'react-navigation';
-import {WebView} from 'react-native-webview';
-import {Image} from 'react-native';
-import {ActivityIndicator, TouchableOpacity, View} from '@components/core';
+import { WebView } from 'react-native-webview';
+import { Image } from 'react-native';
+import { ActivityIndicator, TouchableOpacity, View } from '@components/core';
 import { MAIN_WEBSITE } from '@src/constants/config';
 import chevronLeft from '@assets/images/icons/chevron-left-icon.png';
+import BackButton from '@src/components/BackButton';
+import LogManager from '@src/services/LogManager';
+import LocalDatabase from '@src/utils/LocalDatabase';
+import { CircleBack } from '@src/components/Icons';
+import NavigationService from '@src/services/NavigationService';
 import styles from './style';
 
 const Community = ({ navigation, isFocused }) => {
   const webViewRef = useRef();
   const [loading, setLoading] = useState(true);
   const [backable, setBackable] = useState(false);
-  
-  useEffect(()=>{
-  },[]);
-  if (isFocused) {
-    const { uri: _uri } = navigation?.state?.params || {};
+  const [url, setUrl] = useState('');
 
-    if (_uri) {
-      webViewRef?.current?.injectJavaScript(`location.href = '${_uri}';`);
-      navigation?.setParams({ uri: null });
+  useEffect(() => {
+    if (isFocused) {
+      const { uri: _uri } = navigation?.state?.params || {};
+
+      if (_uri) {
+        webViewRef?.current?.injectJavaScript(`location.href = '${_uri}';`);
+        navigation?.setParams({ uri: null });
+      }
     }
-  }
+    // Check if the cache is existing
+    LocalDatabase.getUriWebviewCommunity()
+      .then(val => {
+        if (val) {
+          setUrl(val);
+        } else {
+          setUrl(MAIN_WEBSITE);
+        }
+      })
+      .catch(err => {
+        setUrl(MAIN_WEBSITE);
+      });
+  }, []);
+
 
   const goBack = () => {
     webViewRef.current.goBack();
@@ -39,12 +58,17 @@ const Community = ({ navigation, isFocused }) => {
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={() => NavigationService.goBack()} style={styles.customBackContainer}>
+        <CircleBack />
+      </TouchableOpacity>
       <WebView
-        onLoadEnd={() => {
+        onLoadEnd={(data) => {
           setLoading(false);
         }}
+        cacheEnabled
+        cacheMode='LOAD_CACHE_ONLY'
         userAgent="Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1"
-        source={{ uri: MAIN_WEBSITE }}
+        source={{ uri: url }}
         ref={webViewRef}
         onNavigationStateChange={stateHandler}
         injectedJavaScript={`
@@ -66,14 +90,16 @@ const Community = ({ navigation, isFocused }) => {
       
         true;
       `}
-        onMessage={({ nativeEvent: state }) => {
+        onMessage={async ({ nativeEvent: state }) => {
           if (state.data === 'navigationStateChange') {
-            // To display navigation bar at the bottom
+            if (typeof state?.url === 'string') {
+              await LocalDatabase.setUriWebviewCommunity(state?.url);
+            }
           }
         }}
       />
-      { loading && <ActivityIndicator style={styles.loading} />}
-      { backable && (
+      {loading && <ActivityIndicator style={styles.loading} />}
+      {backable && (
         <TouchableOpacity onPress={goBack} style={styles.backBtn}>
           <Image
             style={{
