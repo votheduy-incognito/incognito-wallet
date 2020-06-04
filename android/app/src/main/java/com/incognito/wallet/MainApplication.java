4a2fe2a6b8f1c1b6aed98a29c67cdde2f42e827b
uranashel44 <tan@incognito.org>
2020-06-04 10:53:51 +0700

    Update linking manually

--- android/app/src/main/java/com/incognito/wallet/MainApplication.java
+++ android/app/src/main/java/com/incognito/wallet/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.rumors.reactnativesettings.RNSettingsPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
