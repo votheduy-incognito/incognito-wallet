da48be67ab292d8dbdea6b12b85f588eeaa27c73
jayce <phathoang@incognito.org>
2020-06-16 15:50:33 +0700

    New Setting UI
    + Fix some issue Keychain and Setting

--- src/screens/Setting/Setting.enhance.js
+++ src/screens/Setting/Setting.enhance.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ErrorBoundary from '@src/components/ErrorBoundary';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { compose } from 'recompose';
 import { withLayout_2 } from '@src/components/Layout';
-import { actionFetchDevices } from './Setting.actions';
+import { actionFetchServers } from './Setting.actions';
+import { settingSelector } from './Setting.selector';
 
 const enhance = (WrappedComp) => (props) => {
   const dispatch = useDispatch();
+  const { defaultServerId } = useSelector(settingSelector);
   React.useEffect(() => {
-    dispatch(actionFetchDevices());
-  }, []);
+    dispatch(actionFetchServers());
+  }, [defaultServerId]);
   return (
     <ErrorBoundary>
       <WrappedComp {...props} />
--- src/screens/Setting/features/Keychain/Keychain.enhance.js
+++ src/screens/Setting/features/Keychain/Keychain.enhance.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ErrorBoundary from '@src/components/ErrorBoundary';
-import { useSelector, useDispatch } from 'react-redux';
-import { settingSelector } from '@screens/Setting/Setting.selector';
-import { actionFetchServers } from '@screens/Setting/Setting.actions';
+import { useDispatch } from 'react-redux';
+import { actionFetchDevices } from '@screens/Setting/Setting.actions';
 
 const enhance = (WrappedComp) => (props) => {
-  const { defaultServerId } = useSelector(settingSelector);
   const dispatch = useDispatch();
   React.useEffect(() => {
-    dispatch(actionFetchServers());
-  }, [defaultServerId]);
+    dispatch(actionFetchDevices());
+  }, []);
   return (
     <ErrorBoundary>
       <WrappedComp {...props} />
