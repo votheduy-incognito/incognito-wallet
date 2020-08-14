054fa7098966c5369574a9a5e4401d30b4bc5923
0xSiris <siris@incognito.org>
2020-08-14 10:30:27 +0700

    Revert "Merge pull request #1034 from incognitochain/feature/invest-reskin"
    This reverts commit e8d3ac2217f308152aa13250fb4ef0c08353cdf9, reversing
    changes made to f2958dbf5b630e9f98f41a60cf138565fda93113.

--- src/screens/Dex/InvestBalance/ManageBalance/index.js
+++ src/screens/Dex/InvestBalance/ManageBalance/index.js
@@ -87,7 +87,7 @@ const ManageBalance = ({
               style={[mainStyle.flex, mainStyle.button, mainStyle.margin]}
               title="Withdraw"
               onPress={handleWithdraw}
-              disabled={followingCoins.some(coin => !coin.displayBalance) || followingCoins.every(coin => !coin.balance)}
+              disabled={followingCoins.some(coin => !coin.displayBalance)}
             />
           </Row>
           <CoinList coins={followingCoins} />
