79458484d5a7d3b4049befa8b7b5b73e8f0adf0e
0xSiris <tienduong@autonomous.nyc>
2020-08-18 15:36:03 +0700

    Fix uniswap history
    # Conflicts:
    #	src/models/pDefi.js

--- src/models/pDefi.js
+++ src/models/pDefi.js
@@ -56,37 +56,42 @@ export class PDexTradeHistoryModel {
       'Unsuccessful',
       'Successful',
     ][json.Status];
+    this.exchange = TYPES[json.Type] || 'Incognito';
 
     let buyToken = allTokens.find(token => token.id === this.buyTokenId || token.address === this.buyTokenId);
     const sellToken = allTokens.find(token => token.id === this.sellTokenId || token.address === this.sellTokenId);
     const networkFeeToken = allTokens.find(token => token.id === this.networkFeeTokenId);
 
     if (buyToken?.address && sellToken?.address) {
-      this.exchange = 'Kyber';
-      // if (this.networkFee > DEFI_TRADING_FEE) {
-      //   this.tradingFee = DEFI_TRADING_FEE;
-      // } else if (this.networkFee > 4e8) {
-      //   this.tradingFee = 4e8;
-      // } else {
-      //   this.tradingFee = 1e7;
-      // }
       this.tradingFee = (this.networkFee - MAX_DEX_FEE) + MAX_FEE_PER_TX;
       this.networkFee = this.networkFee - this.tradingFee;
 
       const originalSellAmount = BigNumber(this.sellAmount)
         .dividedBy(BigNumber(10).pow(sellToken.pDecimals));
-      const originalPrice = BigNumber(this.buyAmount)
-        .dividedBy(BigNumber(10).pow(18));
-      this.buyAmount = BigNumber(originalPrice)
-        .multipliedBy(originalSellAmount)
+
+      // Decimals is 18 for all buy tokens returned by Kyber/Uniswap (handled by API)
+      this.buyAmount = BigNumber(this.buyAmount);
+
+      // Buy amount of Kyber is expected rate
+      if (this.exchange === 'Kyber') {
+        this.buyAmount = this.buyAmount
+          .dividedBy(BigNumber(10).pow(18))
+          .multipliedBy(originalSellAmount);
+      }
+
+      // Buy amount of Uniswap is minimum amount
+      if (this.exchange === 'Uniswap') {
+        this.buyAmount = this.buyAmount
+          .dividedBy(BigNumber(10).pow(buyToken.decimals));
+      }
+
+      this.buyAmount = this.buyAmount
         .multipliedBy(BigNumber(10).pow(buyToken.pDecimals))
         .toFixed(0);
 
       // this.buyAmount = (this.buyAmount / sellDecimals) * (this.sellAmount / sellPDecimals) * buyDecimals;
       // this.buyAmount = this.buyAmount / Math.pow(10, buyToken.decimals - buyToken.pDecimals);
       // this.buyAmount = Math.floor(this.buyAmount * this.sellAmount / Math.pow(10, sellToken.pDecimals));
-    } else {
-      this.exchange = 'Incognito';
     }
 
     if (buyToken) {
@@ -105,8 +110,6 @@ export class PDexTradeHistoryModel {
       this.networkFee = formatUtil.amountFull(this.networkFee, networkFeeToken.pDecimals);
       this.tradingFee = formatUtil.amountFull(this.tradingFee, networkFeeToken.pDecimals);
     }
-
-    this.exchange = TYPES[json.Type] || 'Incognito';
 
     this.description = `${this.sellAmount} ${this.sellTokenSymbol} to ${this.buyAmount} ${this.buyTokenSymbol}`;
   }
