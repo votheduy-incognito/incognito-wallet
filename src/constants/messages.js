8f9c1fadccdab00f93ad84e64abf09485dc29c1e
jayce <phathoang@incognito.org>
2020-10-01 17:46:52 +0700

    APP-491
    + FIx popup auto close centralized after send in -> unshield
    + Increase time keep popup

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -5,18 +5,14 @@ export default {
   PNODE_UNSTAKE_DESC: 'Stop funded staking',
   ACCOUNT_NOT_FOUND: 'Missing account',
   UNSTAKING: 'unstaking in process',
-
   // error
   CAN_NOT_GET_PDEX_TRADE_HISTORIES: 'Can not get pdex trade histories.',
   CAN_NOT_GET_PDEX_DATA: 'Can not get pdex data.',
   CAN_NOT_GET_POOL_DATA: 'Can not get pool data.',
   DISABLE_ETH_BRIDGE: 'Current congestion may affect out-network functionality. Please check back later.',
-
   CAN_NOT_GET_POOL_HISTORIES: 'Can not get pool histories.',
-
   INSUFFICIENT_BALANCE: 'Your balance is insufficient.',
   NOT_ENOUGH_NETWORK_FEE: 'Please top up PRV to cover the network fee.',
-
   CHANGE_WIFI_SUCCESSFULLY_TITLE: 'Success!',
   CHANGE_WIFI_SUCCESSFULLY_DESCRIPTION: 'You changed your Node’s Wi-Fi network.',
   CAN_NOT_CHANGE_WIFI: 'Change wifi unsuccessfully. Please try again.',
@@ -27,4 +23,5 @@ export default {
   NODE_NOT_SUPPORTED_UPDATE_WIFI_TITLE: 'Please contact support',
   NODE_NOT_SUPPORTED_UPDATE_WIFI_DESCRIPTION: 'Your Node is not up to date. Please reach out to go@incognito.org for support.',
   UPDATE_WIFI_INSTRUCTION: 'Please turn off cellular data on your device. Using Wi-Fi only, make sure your Node is online before proceeding to change networks.',
+  PENDING_TRANSACTIONS: 'Please wait for your previous transaction to finish processing. Simply try again later.',
 };
