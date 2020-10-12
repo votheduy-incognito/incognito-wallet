891b973aac3700bb9c547f3e4b8c6af994a1cc62
SangNX063 <sang@incognito.org>
2020-10-12 15:53:41 +0700

    APP-303: User taps a notification, user can use app although home config is disable

--- src/shared/hooks/featureConfig.js
+++ src/shared/hooks/featureConfig.js
@@ -3,12 +3,30 @@ import Toast from '@components/core/Toast/Toast';
 import { useFocusEffect } from 'react-navigation-hooks';
 import { getFunctionConfigs } from '@services/api/misc';
 
+export const getDurationShowMessage = (message) => {
+  message = message || '';
+  return Math.max(Math.floor(message.length / 15) * 1000, 2000);
+};
+
+export const handleGetFunctionConfigs = async (featureName) => {
+  try {
+    const features = await getFunctionConfigs();
+    if (features && features.length) {
+      return features.find(featureItem => featureItem.name === featureName) || {};
+    } else {
+      return{};
+    }
+  } catch (e) {
+    console.debug('CAN NOT GET FEATURE', featureName, e);
+  }
+};
+
 function useFeatureConfig(featureName, onPress) {
   const [feature, setFeature] = useState(null);
 
   const handlePress = useCallback((...params) => {
     if (feature && feature.disabled) {
-      const duration = Math.max(Math.floor(feature.message.length / 15) * 1000, 2000);
+      const duration = getDurationShowMessage(feature.message);
       return Toast.showInfo(feature.message, {
         duration
       });
@@ -29,13 +47,8 @@ function useFeatureConfig(featureName, onPress) {
 
   const getFeature = async () => {
     try {
-      const features = await getFunctionConfigs();
-      if (features && features.length) {
-        const feature = features.find(featureItem => featureItem.name === featureName);
-        setFeature(feature);
-      } else {
-        setFeature({});
-      }
+      const feature = await handleGetFunctionConfigs(featureName);
+      setFeature(feature);
     } catch (e) {
       console.debug('CAN NOT GET FEATURE', featureName, e);
     }
