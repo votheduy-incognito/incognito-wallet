48ed749ad8fbb6acdc0bc2dc1525f50adc446058
jayce <phathoang@incognito.org>
2020-10-19 14:03:50 +0700

    Receive History
    + Improve load more data

--- src/components/HistoryList/HistoryList.useEffect.js
+++ src/components/HistoryList/HistoryList.useEffect.js
@@ -6,37 +6,18 @@ import { useFocusEffect } from 'react-navigation-hooks';
 
 export const useHistoryList = (props) => {
   const { handleLoadHistory } = props;
-  const {
-    histories,
-    isFetched: isFetchedMergeHistory,
-    isFetching: isFetchingMergeHistory,
-    refreshing: refreshingHistory,
-  } = useSelector(tokenSeleclor.historyTokenSelector);
   selectedPrivacySeleclor.selectedPrivacy;
   const receiveHistory = useSelector(tokenSeleclor.receiveHistorySelector);
-  const {
-    isFetching,
-    isFetched,
-    oversize,
-    refreshing: refreshingReceiveHistory,
-  } = receiveHistory;
-  const refreshing = refreshingHistory || refreshingReceiveHistory;
-  const showEmpty =
-    histories?.length === 0 && isFetchedMergeHistory && !isFetchingMergeHistory;
+  const { notEnoughData, showEmpty, refreshing } = receiveHistory;
+  const { histories } = useSelector(tokenSeleclor.historyTokenSelector);
   React.useEffect(() => {
-    const notEnoughData =
-      histories?.length < 10 &&
-      !isFetching &&
-      isFetched &&
-      !oversize &&
-      !refreshing;
     if (notEnoughData) {
-      handleLoadHistory();
+      handleLoadHistory(false, true);
     }
   }, [histories, receiveHistory]);
   useFocusEffect(
     React.useCallback(() => {
-      handleLoadHistory(true);
+      handleLoadHistory(true, false);
     }, []),
   );
   return [showEmpty, refreshing];
