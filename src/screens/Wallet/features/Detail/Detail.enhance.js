1f290a905f8f0d8ebf6bda7287aa84a347f0c1c7
jayce <phathoang@incognito.org>
2020-10-21 11:45:10 +0700

    APP-160: [Receive]: Detail token is loaded very slow when user has more records in history detail token

--- src/screens/Wallet/features/Detail/Detail.enhance.js
+++ src/screens/Wallet/features/Detail/Detail.enhance.js
@@ -4,18 +4,37 @@ import { withLayout_2 } from '@src/components/Layout';
 import { compose } from 'recompose';
 import { useFocusEffect } from 'react-navigation-hooks';
 import withWallet from '@screens/Wallet/features/Home/Wallet.enhance';
+import { useDispatch } from 'react-redux';
+import {
+  actionFreeReceiveHistory,
+  actionFreeHistory,
+} from '@src/redux/actions/token';
 
 const enhance = (WrappedComp) => (props) => {
   const {
     retryLastTxsUnshieldDecentralized,
     retryLastTxsUnshieldCentralized,
   } = props;
+  const dispatch = useDispatch();
+  const handleFreeHistoryData = () => {
+    try {
+      dispatch(actionFreeReceiveHistory());
+      dispatch(actionFreeHistory());
+    } catch (error) {
+      console.debug('ERROR', error);
+    }
+  };
   useFocusEffect(
     React.useCallback(() => {
       retryLastTxsUnshieldCentralized();
       retryLastTxsUnshieldDecentralized();
     }, []),
   );
+  React.useEffect(() => {
+    return () => {
+      handleFreeHistoryData();
+    };
+  }, []);
   return (
     <ErrorBoundary>
       <WrappedComp {...{ ...props }} />
