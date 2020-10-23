899aacb4c063c808ac73dcfa430afef9bf66c212
SangNX063 <sang@incognito.org>
2020-10-23 10:19:42 +0700

    APP-170: The status is shown wrong when user reload page

--- src/screens/Wallet/features/TxHistoryDetail/TxHistoruDetail.builder.js
+++ src/screens/Wallet/features/TxHistoryDetail/TxHistoruDetail.builder.js
@@ -5,6 +5,7 @@ import { combineHistory } from '@src/redux/utils/token';
 import { selectedPrivacySeleclor } from '@src/redux/selectors';
 import historyModel from '@models/history';
 import _ from 'lodash';
+import { COLORS } from '@src/styles';
 
 const combineHistoryStyle = (state, history) => {
   const account       = defaultAccountSelector(state);
@@ -34,13 +35,24 @@ export const combineHistoryDetail = (state, historyId) => {
 
 export const combineHistoryApi = (state, historyFromApi) => {
   const selectedPrivacy = selectedPrivacySeleclor.selectedPrivacy(state);
+  const parseHistoryApi = historyModel.parsePrivateTokenFromApi(historyFromApi);
   const histories = combineHistory({
     histories: [],
-    historiesFromApi: [historyModel.parsePrivateTokenFromApi(historyFromApi)],
+    historiesFromApi: [parseHistoryApi],
     symbol: selectedPrivacy?.symbol,
     externalSymbol: selectedPrivacy?.externalSymbol,
     decimals: selectedPrivacy?.decimals,
     pDecimals: selectedPrivacy?.pDecimals,
   });
-  return combineHistoryStyle(state, _.head(histories));
+  const history       = combineHistoryStyle(state, _.head(histories));
+  const statusMessage = parseHistoryApi.statusMessage;
+  const complete      = appConstant.STATUS_MESSAGE.COMPLETE.toUpperCase();
+  const statusColor   = statusMessage.toUpperCase().includes(complete)
+    ? COLORS.green
+    : COLORS.colorGreyBold;
+  return {
+    ...history,
+    statusMessage,
+    statusColor
+  };
 };
