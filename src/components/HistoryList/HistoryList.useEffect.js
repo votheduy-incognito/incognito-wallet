ad18a9cc455946be13be5993fed122e34eb538e2
jayce <phathoang@incognito.org>
2020-10-27 15:35:42 +0700

    APP-177: History List not show when tap notification

--- src/components/HistoryList/HistoryList.useEffect.js
+++ src/components/HistoryList/HistoryList.useEffect.js
@@ -1,24 +1,33 @@
-import { tokenSeleclor, selectedPrivacySeleclor } from '@src/redux/selectors';
+import {
+  tokenSeleclor,
+  selectedPrivacySeleclor,
+  accountSeleclor,
+} from '@src/redux/selectors';
 import { useSelector } from 'react-redux';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useFocusEffect } from 'react-navigation-hooks';
+import { tokensFollowedSelector } from '@src/redux/selectors/token';
+import { walletSelector } from '@src/redux/selectors/wallet';
 
 export const useHistoryList = (props) => {
   const { handleLoadHistory } = props;
-  selectedPrivacySeleclor.selectedPrivacy;
+  const wallet = useSelector(walletSelector);
+  const followed = useSelector(tokensFollowedSelector);
+  const account = useSelector(accountSeleclor.defaultAccountSelector);
+  const selectedPrivacy = useSelector(selectedPrivacySeleclor.selectedPrivacy);
   const receiveHistory = useSelector(tokenSeleclor.receiveHistorySelector);
   const { notEnoughData, showEmpty, refreshing } = receiveHistory;
   const { histories } = useSelector(tokenSeleclor.historyTokenSelector);
   React.useEffect(() => {
     if (notEnoughData) {
-      handleLoadHistory(false, true);
+      handleLoadHistory(false);
     }
   }, [histories, receiveHistory]);
   useFocusEffect(
     React.useCallback(() => {
-      handleLoadHistory(true, false);
-    }, []),
+      handleLoadHistory(true);
+    }, [selectedPrivacy, account, followed, wallet]),
   );
   return [showEmpty, refreshing];
 };
