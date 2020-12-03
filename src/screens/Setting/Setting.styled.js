829e186643d90e31703f01f3c39ac61f9bc7d2e2
0xSiris <siris@incognito.org>
2020-12-03 13:50:20 +0700

    APP-436: Fix settings scroll

--- src/screens/Setting/Setting.styled.js
+++ src/screens/Setting/Setting.styled.js
@@ -2,13 +2,6 @@ import { COLORS, FONT } from '@src/styles';
 import { StyleSheet } from 'react-native';
 
 export const settingStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  extra: {
-    flex: 1,
-    marginTop: 22,
-  },
   textVersion: {
     textAlign: 'center',
     fontFamily: FONT.NAME.bold,
@@ -16,16 +9,5 @@ export const settingStyle = StyleSheet.create({
     lineHeight: FONT.SIZE.small + 3,
     color: COLORS.black,
     marginBottom: 25,
-  },
-  btnBackup: {
-    backgroundColor: COLORS.colorGrey,
-    height: 40,
-    width: 75,
-  },
-  titleBtnBackup: {
-    color: COLORS.black,
-    fontFamily: FONT.NAME.medium,
-    fontSize: FONT.SIZE.regular,
-    lineHeight: FONT.SIZE.regular + 3,
   },
 });
