80da1c1fce3384729ab243e7f4d7bb7ce9c9c3c4
SangNX063 <sang@incognito.org>
2021-01-11 14:14:56 +0700

    APP-504: [Toggle USD]  Price PRV is converted to USD, actually converted to USDT

--- src/redux/utils/selectedPrivacy.js
+++ src/redux/utils/selectedPrivacy.js
@@ -1,38 +1,23 @@
-import isNaN from 'lodash/isNaN';
-
 export const getPrice = ({ token, tokenUSDT }) => {
-  const { pricePrv: priceOneUsdtByPrv } = tokenUSDT;
-  const priceOnePrvToUsdt = 1 / priceOneUsdtByPrv;
   const defaultValue = {
     pricePrv: 0,
     change: '0',
+    priceUsd: 0,
   };
-  if (!tokenUSDT || !priceOnePrvToUsdt) {
+  if (!tokenUSDT) {
     return defaultValue;
   }
+  const { pricePrv, priceUsd } = tokenUSDT;
   if (token?.isMainCrypto) {
     return {
       change: '0',
       pricePrv: 1,
-      priceUsd: priceOnePrvToUsdt || 0,
+      priceUsd: priceUsd / pricePrv || 0,
     };
   }
-  const _pricePrv =
-    Number(token?.pricePrv) !== 0
-      ? token?.pricePrv
-      : token?.priceUsd / priceOnePrvToUsdt;
-  const _priceUsd =
-    Number(token?.priceUsd) !== 0
-      ? token?.priceUsd
-      : _pricePrv * priceOnePrvToUsdt;
   return {
     change: token?.change || defaultValue.change,
-    pricePrv:
-      token?.pricePrv !== 0
-        ? token?.pricePrv
-        : isNaN(_pricePrv)
-          ? 0
-          : _pricePrv,
-    priceUsd: _priceUsd,
+    pricePrv: token?.pricePrv !== 0 ? token?.pricePrv : 0,
+    priceUsd: token?.priceUsd !== 0 ? token?.priceUsd : 0,
   };
 };
