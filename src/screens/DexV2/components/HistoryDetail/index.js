7964acb072f56a9723843c6c867fb950eec251c6
SangNX063 <sang@incognito.org>
2021-01-12 11:59:30 +0700

    APP-105: Add a Price rate field in the History screen.

--- src/screens/DexV2/components/HistoryDetail/index.js
+++ src/screens/DexV2/components/HistoryDetail/index.js
@@ -8,6 +8,8 @@ import ExtraInfo from '@screens/DexV2/components/ExtraInfo';
 import { CONSTANT_COMMONS } from '@src/constants';
 import HuntQRCode from '@components/HuntQRCode/HuntQRCode';
 import { HISTORY_STATUS } from '@src/constants/trading';
+import BigNumber from 'bignumber.js';
+import formatUtils from '@utils/format';
 import styles from './style';
 import withData from './data.enhance';
 
@@ -16,6 +18,11 @@ const HistoryDetail = ({ history }) => {
   && history?.amountReceive
     ? history?.amountReceive
     : history?.buyAmount;
+
+  const priceRate = formatUtils
+    .fixedNumber(new BigNumber(buyAmount)
+      .dividedBy(BigNumber(history.sellAmount))
+      .toNumber(), 9);
 
   let factories = [
     {
@@ -54,6 +61,10 @@ const HistoryDetail = ({ history }) => {
     {
       left: 'Exchange',
       right: history?.exchange,
+    },
+    !isNaN(priceRate) && {
+      left: 'Price rate',
+      right: priceRate,
     },
   ];
   factories = factories.map((item) => ({
